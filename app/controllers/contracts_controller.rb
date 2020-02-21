36fc77aee086b23325e7f5e5245f22bc5288e202
Daekoon <daekoon_lim@hotmail.com>
2020-02-21 08:14:44 +0800

    Create form for generating loan contract

--- app/controllers/contracts_controller.rb
+++ app/controllers/contracts_controller.rb
@@ -1,5 +1,20 @@
 class ContractsController < ApplicationController
+
+  def new
+
+  end
+
+  def create
+
+  end
+
   def show
     @contract = Contract.find(params[:id])
+  end
+
+  def select
+  end
+
+  def generate
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,10 @@ Rails.application.routes.draw do
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
   resources :users,  only: [:show]
-  resources :contracts, only: [:show]
+
+  get '/contracts/select', to: 'contracts#select'
+  post '/contracts/generate', to: 'contracts#generate'
+  resources :contracts, only: [:show, :create, :new]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
 end
